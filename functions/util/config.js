903209c8da7c53bd76e68e9a88a5aaa3b33c1ee3
Charles_CH.HO <34810665+a20041060@users.noreply.github.com>
2020-10-19 17:39:11 +0800

    Create config.js

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyAlOsArThu-HajykgCvxGaFMNdKDvchObU",
+    authDomain: "socialape-1d4e0.firebaseapp.com",
+    databaseURL: "https://socialape-1d4e0.firebaseio.com",
+    projectId: "socialape-1d4e0",
+    storageBucket: "socialape-1d4e0.appspot.com",
+    messagingSenderId: "1061107121202",
+    appId: "1:1061107121202:web:ee6b5335b852cfdc86d02b"
+  }; 
